c07649fb5507ea462cff77e15a78d92dae2a7445
Lahiru Supun <lhrsupun@gmail.com>
2022-08-25 00:38:10 +0530

    user creation added

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,52 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { ObjectId } = require("mongodb");
+
+const User = require('../models/User');
+
+const {
+    getToken,
+    verifyToken,
+    encryptPassowrd,
+} = require('../middleware/common-functions')
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+
+        let {
+            email,
+            firstName,
+            lastName,
+            password
+        } = req.body;
+
+        email = email.toLowerCase();
+
+        const existsUser = await User.findOne({ email });
+        if (existsUser) {
+            return res.status(401).json("Email already exists!");
+        }
+
+        const verifyTokens = verifyToken();
+
+        const newUser = new User({ firstName, lastName, email });
+
+        newUser.password = await encryptPassowrd(password);
+        newUser.verificationToken = verifyTokens.verificationToken;
+        newUser.verificationTokenTimeStamp = verifyTokens.verificationTokenTimeStamp;
+
+        await newUser.save();
+        return res.status(200).json({
+            message: "User Created Successfully"
+        })
+    } catch (error) {
+        console.log({ error });
+        return res.status(500).json("500 Internal Server Error");
+    }
+});
+
 
 router.get('/', async (req, res) => {
     try {
