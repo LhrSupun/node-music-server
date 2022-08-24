08ae44c5055d0d8224ea53eacb062f180d276889
Lahiru Supun <lhrsupun@gmail.com>
2022-08-25 00:38:26 +0530

    user Model created

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+        },
+        firstName: String,
+        lastName: String,
+        password: String,
+        lastSeen: {
+            type: Date
+        },
+        verificationTokenTimeStamp: {
+            type: Number,
+            default: 0
+        },
+        verificationToken: String,
+    },
+    {
+        timestamps: true,
+        toObject: {
+            virtuals: true,
+        },
+        toJSON: {
+            virtuals: true,
+        },
+    });
+UserSchema.virtual('fullName').get(() => `${this.firstName} ${this.lastName ? this.lastName : ""}`)
+
+
+module.exports = mongoose.model("User", UserSchema);
